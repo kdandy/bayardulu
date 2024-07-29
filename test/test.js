602d3b54a019a371ecb1e7e259b360ebe931e086
pentesting <dandybdr@gmail.com>
2024-07-29 22:47:35 +0700

    fetch: first commit

--- test/test.js
+++ test/test.js
@@ -3,26 +3,28 @@ const fs = require('fs');
 const path = require('path');
 
 const testFilePath = path.join(__dirname, 'dummy.txt');
-const encryptedFilePath = `${testFilePath}.enc`;
+const encryptedFilePath = `${testFilePath}.bayardulu`;
 const decryptedFilePath = `${testFilePath}.dec`;
 
-// Buat file dummy untuk pengujian
 fs.writeFileSync(testFilePath, 'Ini adalah file dummy untuk pengujian.');
 
-exec(`node bin/bayardulu.js encrypt ${testFilePath}`, (err, stdout, stderr) => {
+exec(`node bin/bayardulu.js encrypt ${testFilePath} -x bayardulu`, (err, stdout, stderr) => {
   if (err) {
     console.error(`Encrypt Error: ${stderr}`);
     process.exit(1);
   } else {
     console.log(`Encrypt Output: ${stdout}`);
-    exec(`node bin/bayardulu.js decrypt ${encryptedFilePath}`, (err, stdout, stderr) => {
+    
+    exec(`node bin/bayardulu.js decrypt ${encryptedFilePath} -k N7wKWb5434FLD -o`, (err, stdout, stderr) => {
       if (err) {
         console.error(`Decrypt Error: ${stderr}`);
         process.exit(1);
       } else {
         console.log(`Decrypt Output: ${stdout}`);
+        
         const originalContent = fs.readFileSync(testFilePath, 'utf8');
-        const decryptedContent = fs.readFileSync(decryptedFilePath, 'utf8');
+        const decryptedContent = fs.readFileSync(testFilePath, 'utf8');
+        
         if (originalContent === decryptedContent) {
           console.log('Test passed: decrypted content matches original content');
           process.exit(0);
